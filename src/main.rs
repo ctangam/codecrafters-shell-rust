cfd8435ab9ac991d0be7a056b8152e3fb13bee5f
ctangam <luochenjoey@gmail.com>
2025-06-19 18:02:30 +0800

    exit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     loop {
@@ -11,7 +12,10 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        match input {
+        match &input[..] {
+            "exit 0" => {
+                exit(0)
+            }
             input => {
                 println!("{}: command not found", input.trim())
             }
