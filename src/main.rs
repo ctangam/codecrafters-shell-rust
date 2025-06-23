df56297e27d0be53f51945cc2db808febd4e11a8
ctangam <luochenjoey@gmail.com>
2025-06-23 11:16:29 +0800

    add redirect

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,18 @@ use std::io::{self, Write};
 use std::{
     env, fs,
     path::Path,
-    process::{exit, Command},
+    process::{exit, Command, Stdio},
 };
 
 use anyhow::Result;
+
+enum Symbol {
+    Single(String),
+    Double(String),
+    Normal(String),
+    Stdout(String),
+    Whitespace,
+}
 
 fn main() -> Result<()> {
     loop {
@@ -29,53 +37,67 @@ fn main() -> Result<()> {
         if input.is_empty() {
             continue;
         }
-        let mut input = parse(input).into_iter();
-        let cmd = input.next().unwrap_or_default();
-        let mut args = input.skip(1);
+        let (cmd, mut args, stdout, stderr) = parse(input);
+
         match &cmd[..] {
             "pwd" => {
                 println!("{}", env::current_dir()?.display());
             }
             "cd" => {
-                let target = args.next().unwrap_or_default();
-                if target == "~" {
-                    env::set_current_dir(home)?;
-                } else if fs::exists(&target)? {
-                    env::set_current_dir(target)?;
-                } else {
-                    println!("cd: {}: No such file or directory", target)
+                if let Some(target) = args.first() {
+                    if target == "~" {
+                        env::set_current_dir(home)?;
+                    } else if fs::exists(&target)? {
+                        env::set_current_dir(target)?;
+                    } else {
+                        println!("cd: {}: No such file or directory", target)
+                    }
                 }
             }
             "echo" => {
-                let args = args.collect::<String>();
+                let args = args.concat();
                 println!("{}", args)
             }
             "type" => {
-                let cmd = args.next().unwrap_or_default();
-                match &cmd[..] {
-                    "echo" | "exit" | "type" | "pwd" | "cd" => {
-                        println!("{} is a shell builtin", cmd)
-                    }
-                    _ => {
-                        if let Some(path) = search(paths, &cmd) {
-                            println!("{} is {}", cmd, path);
-                        } else {
-                            println!("{}: not found", cmd);
+                if let Some(cmd) = args.first() {
+                    match &cmd[..] {
+                        "echo" | "exit" | "type" | "pwd" | "cd" => {
+                            println!("{} is a shell builtin", cmd)
+                        }
+                        _ => {
+                            if let Some(path) = search(paths, &cmd) {
+                                println!("{} is {}", cmd, path);
+                            } else {
+                                println!("{}: not found", cmd);
+                            }
                         }
                     }
                 }
             }
             "exit" => {
-                let code = args.next().unwrap_or_default().parse().unwrap_or_default();
+                let code = args.first().map_or(Ok(0), |s| s.parse())?;
                 exit(code)
             }
             cmd => {
-                if let Some(_path) = search(paths, &cmd) {
-                    let args = args
-                        .into_iter()
-                        .filter(|s| !s.is_empty() && !s.trim().is_empty())
-                        .collect::<Vec<String>>();
-                    let mut child = Command::new(cmd).args(args).spawn()?;
+                if let Some(_path) = search(paths, cmd) {
+                    args.retain(|s| !s.is_empty() && !s.trim().is_empty());
+                    let stdout = if let Some(stdout) = stdout {
+                        let fd = fs::File::open(stdout)?;
+                        Stdio::from(fd)
+                    } else {
+                        Stdio::inherit()
+                    };
+                    let stderr = if let Some(stderr) = stderr {
+                        let fd = fs::File::open(stderr)?;
+                        Stdio::from(fd)
+                    } else {
+                        Stdio::inherit()
+                    };
+                    let mut child = Command::new(cmd)
+                        .args(args)
+                        .stdout(stdout)
+                        .stderr(stderr)
+                        .spawn()?;
                     let _ = child.wait();
                 } else {
                     println!("{}: not found", cmd);
@@ -85,16 +107,43 @@ fn main() -> Result<()> {
     }
 }
 
-fn parse(input: &str) -> Vec<String> {
+fn parse(input: &str) -> (String, Vec<String>, Option<String>, Option<String>) {
     let input = input.chars().collect::<Vec<char>>();
-    let mut args = Vec::new();
     let mut i = 0;
+    let mut cmd = String::new();
+    while i < input.len() && input[i] != ' ' {
+        cmd.push(input[i]);
+    }
+    i += 1;
+    let mut args = Vec::new();
+    let mut stdout = None;
+    let mut stderr = None;
     loop {
         if i >= input.len() {
             break;
         }
         let mut s = String::new();
         match input[i] {
+            '>' => {
+                i += 2;
+                while i < input.len() {
+                    s.push(input[i])
+                }
+                stdout = Some(s);
+                break;
+            }
+            '1' if input[i + 1] == '>' => {
+                i += 1;
+                continue;
+            }
+            '2' if input[i + 1] == '>' => {
+                i += 3;
+                while i < input.len() {
+                    s.push(input[i])
+                }
+                stderr = Some(s);
+                break;
+            }
             '"' => {
                 i += 1;
                 while i < input.len() && input[i] != '"' {
@@ -154,39 +203,8 @@ fn parse(input: &str) -> Vec<String> {
         }
         args.push(s);
     }
-    args
-}
 
-fn parse_args(args: &str) -> Vec<String> {
-    if args.contains('"') {
-        args.split('"')
-    } else {
-        args.split('\'')
-    }
-    .enumerate()
-    .flat_map(|(n, s)| {
-        if n % 2 == 1 {
-            vec![s.to_string()]
-        } else {
-            if !s.is_empty() && s.trim().is_empty() {
-                vec![" ".to_string()]
-            } else {
-                let start = s.starts_with(' ');
-                let end = s.ends_with(' ');
-                let s = s
-                    .split_ascii_whitespace()
-                    .collect::<Vec<&str>>()
-                    .join(" ")
-                    .replace("\\", "");
-                let s = if start { format!(" {}", s) } else { s };
-                let s = if end { format!("{} ", s) } else { s };
-                s.split_inclusive(' ')
-                    .map(|s| s.to_string())
-                    .collect::<Vec<String>>()
-            }
-        }
-    })
-    .collect::<Vec<String>>()
+    (cmd, args, stdout, stderr)
 }
 
 fn search<T>(paths: T, cmd: &str) -> Option<String>
